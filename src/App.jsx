e93f0febfd9f1c7fe3edbd4ed6bd965756a2ace0
Jon Greenspan <jongreenspan37@gmail.com>
2026-08-07 07:00:41 +0100

    initial setup vite react react router tailwind basic home page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
